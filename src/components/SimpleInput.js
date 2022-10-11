40574dfcaf73c3a924830eb00e75cf87ab22391b
Mounir Bendahmane <mounirbendahmane@MacBook-Pro-de-Mounir-2.local>
2022-10-11 11:56:50 +0200

    blur and form validity control not working

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,13 +1,71 @@
+import { useRef, useState, useEffect } from 'react'
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [nameState, setNameState] = useState('default')
+  const [nameIsValid, setNameIsValid] = useState(false)
+  const [nameIsTouched, setNameIsTouched] = useState(false)
+
+  useEffect(()=>{
+      if (nameIsValid) console.log('name is Valid')
+  }, [nameIsValid])
+
+  const nameChangeHandler = (e) => {
+    setNameState(e.target.value)
+  }
+
+  const nameBlurHandler = (e) => {
+    setNameIsTouched(true);
+
+    if(nameState.trim() === '') {
+      setNameIsValid(false)
+      return;
+    }
+
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault()
+    setNameIsTouched(true);
+
+    if(nameState.trim() === '') {
+      setNameIsValid(false)
+      return;
+    }
+    setNameIsValid(true);
+
+
+    // .... traitement exemple: requete POST
+
+    
+    setNameState('');
+  }
+
+
+  const nameInputIsinvalid = !nameIsValid &&  nameIsTouched;
+  const nameInputClasses = nameInputIsinvalid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input 
+        value={nameState}
+        onChange={nameChangeHandler} 
+        onBlur={nameBlurHandler}
+        type='text' 
+        id='name' />
+
+        {nameIsValid && (
+          <p className='error-text'>Name cannot be empty</p>
+        )}
+
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button >Submit</button>
       </div>
+
+      {nameState}
     </form>
   );
 };
